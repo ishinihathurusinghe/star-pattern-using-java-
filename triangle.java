c71d6605937cbb00a879f2a16ca4480b39dbf907
Ishini Hathurusinghe <101358580+ishinihathurusinghe@users.noreply.github.com>
2022-09-12 21:29:41 +0530

    Add files via upload

---
+++ triangle.java
@@ -0,0 +1,14 @@
+    public class triangle    {   
+    public static void main(String args[])   
+    {   
+        for(int i=1; i<=4; i++)   
+        {   
+        for(int j=1; j<=i; j++)   
+        {   
+        System.out.print("*");   
+        }   
+        System.out.println();  
+    }  
+    System.out.print("**** ");
+    }
+}
