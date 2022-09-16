7fe5de63b95d95eb979c8140da3cbd3713b2ff8f
Ishini Hathurusinghe <101358580+ishinihathurusinghe@users.noreply.github.com>
2022-09-16 15:54:03 +0530

    Update triangle.java

--- triangle.java
+++ triangle.java
@@ -1,4 +1,4 @@
-    public class triangle                                         {   
+    public class triangle                                          {   
     public static void main(String args[])   
     {   
         for(int i=1; i<=4; i++)   
